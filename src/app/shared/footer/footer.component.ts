66d20589e20d8ae94585fc3389a40bf31634ac5c
Bilal Alac <bilalalac@gmail.com>
2025-11-05 21:34:41 +0100

    fixes responsive view on skills-section and footer

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -5,7 +5,7 @@ import { TranslateModule } from '@ngx-translate/core';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss'],
+  styleUrls: ['./footer.component.scss', '../responsive/responsive-footer.scss'],
   imports: [TranslateModule],
   standalone: true,
 })
